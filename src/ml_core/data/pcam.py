8d94e90fe353f0992db97879a4d05e13a0043f32
Mau13-dot <mauritsdoeswil@hotmail.com>
2026-01-11 12:03:26 +0100

    Implemented loader and pcam py

--- src/ml_core/data/pcam.py
+++ src/ml_core/data/pcam.py
@@ -18,20 +18,40 @@ class PCAMDataset(Dataset):
         self.transform = transform
 
         # TODO: Initialize dataset
-        # 1. Check if files exist
+        if not self . x_path . exists () or not self . y_path . exists ():
+            raise FileNotFoundError (
+            f" PCAM files not found at { self . x_path } or { self . y_path }
+            "   
+            )
+
         # 2. Open h5 files in read mode
-        pass
+        self . x_data = h5py . File ( self .x_path , "r")["x"]
+        self . y_data = h5py . File ( self .y_path , "r")["y"]
+
 
     def __len__(self) -> int:
         # TODO: Return length of dataset
+        def __len__ ( self ) -> int:
+            return len( self . x_data )
         # The dataloader will know hence how many batches to create
-        return 0
+
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+
         # TODO: Implement data retrieval
         # 1. Read data at idx
+        image = self . x_data [idx]
+        label = self . y_data [idx ][0]
+
         # 2. Convert to uint8 (for PIL compatibility if using transforms)
+        image = image . astype (np. uint8 )
+
         # 3. Apply transforms if they exist
+        if self . transform :
+            image = self . transform ( image )
+
         # 4. Return tensor image and label (as long)
+        return image , torch . tensor (label , dtype = torch . long ). squeeze ()
+
         
-        raise NotImplementedError("Implement __getitem__ in PCAMDataset")
+        
